ad5ec56690b2c093d2cf6bb2162f71667d8a03c0
lankokelly <lankokelly@gmail.com>
2018-07-11 14:42:19 +0800

    ii

---
+++ Lesson 3/Exercise3 judge_big_lower.py
@@ -0,0 +1,10 @@
+#Ex3 compare 2 number and print the output
+def compare(C1,C2):
+    if C1 > C2 :
+        print ("a > b")
+    elif C1 < C2 :
+        print ("a > b")
+    else :
+        print ("a = b")
+compare (1,2) #try two different interger
+compare (3,3) #try two same interger
