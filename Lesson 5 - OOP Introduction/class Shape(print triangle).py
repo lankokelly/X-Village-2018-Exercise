5957f2c0ea03a3414125c465a7eca2d09634239e
lankokelly <lankokelly@gmail.com>
2018-07-13 11:41:32 +0800

    i

--- Lesson 5 - OOP Introduction/class Shape(print triangle).py
+++ Lesson 5 - OOP Introduction/class Shape(print triangle).py
@@ -5,20 +5,8 @@ class Shape:
         self.edge = edge_arg
         
     def display(self):
-class Shape: 
-    edge = 'default_edge'
-    
-    def set_edge(self, edge_arg):
-        self.edge = edge_arg
-        
-    def display(self):
         for i in range(1,self.edge +1):
             print( "*" * i )
-
-
-x = Shape()
-x.set_edge(5)
-x.display()
 
 
 x = Shape()
